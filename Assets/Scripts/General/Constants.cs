e2545c9aa73e721fd5405128593526e6d92c750d
JabukeGrada <dinometaelasticglass@gmail.com>
2015-10-14 23:25:47 +0900

    Skinned Background TEH created + added test environment for it

---
+++ Assets/Scripts/General/Constants.cs
@@ -0,0 +1,7 @@
+﻿public partial class General 
+{
+    public partial class Constants
+    {
+        public const string DocumentationRoot = "Signa1992.github.io/Mo_2015_11_30_Bichik/Docs/html/";
+    }
+}
